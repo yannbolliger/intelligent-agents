b20bb79884e32a4cfa1e5b4493be4ef4a5210c09
kyle gerard <kyle.g@hotmail.fr>
2018-09-25 16:48:41 +0200

    different default params

--- rabbits/bolliger-gerard-ex1/src/RabbitsGrassSimulationModel.java
+++ rabbits/bolliger-gerard-ex1/src/RabbitsGrassSimulationModel.java
@@ -30,8 +30,8 @@ public class RabbitsGrassSimulationModel extends SimModelImpl {
 
     public static final int DEFAULT_GRID_SIZE = 20;
     public static final int DEFAULT_NUMBER_RABBITS = 100;
-    public static final int DEFAULT_BIRTH_THRESHOLD = 15;
-    public static final int DEFAULT_BIRTH_ENERGY = 5;
+    public static final int DEFAULT_BIRTH_THRESHOLD = 40;
+    public static final int DEFAULT_BIRTH_ENERGY = 30;
     public static final int DEFAULT_GROWTH_RATE  = 5;
     public static final int DEFAULT_GRASS_ENERGY = 5;
 
