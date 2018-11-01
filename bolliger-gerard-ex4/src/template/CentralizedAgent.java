db5575497add6c2cc3f280e4269aff1256228cce
Yann Bolliger <yann.bolliger@epfl.ch>
2018-11-01 15:36:16 +0100

    Smaller p value.

--- bolliger-gerard-ex4/src/template/CentralizedAgent.java
+++ bolliger-gerard-ex4/src/template/CentralizedAgent.java
@@ -23,7 +23,7 @@ import logist.topology.Topology;
  */
 
 public class CentralizedAgent implements CentralizedBehavior {
-    private static final double P = 0.5;
+    private static final double P = 0.3;
     private static final long DELTA = 3_000;
     private static final int MAX_NEIGHBORS_SIZE = 250_000;
 
